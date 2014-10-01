77e384412c8ffc9f6d6b19412283f9bd4048f0f6
Logan Arnett <logan@loganarnett.com>
2014-09-30 20:14:02 -0400

    @LoganArnett & @rgcoders

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -29,6 +29,21 @@ var letters2Numbers = function(letters){
    if (letters == "five"){
      letters = 5;
    }
+   if (letters == "six"){
+     letters = 6;
+   }
+   if (letters == "seven"){
+     letters = 7;
+   }
+   if (letters == "eight"){
+     letters = 8;
+   }
+   if (letters == "nine"){
+     letters = 9;
+   }
+   if (letters == "ten"){
+     letters = 10;
+   }
    return letters;
 }
 
@@ -41,7 +56,7 @@ var add = function(a, b) {
 };
 
 function test_letters2Numbers(a, b){
-  console.log('it should convert "' + a + '" and "' + b + '" :',
+  console.log('it should convert "' + a + '" to "' + b + '" :',
   letters2Numbers(a) === b);
 }
 
@@ -49,9 +64,42 @@ test_letters2Numbers("one", 1);
 test_letters2Numbers("two", 2);
 
 
-function test_add(){
-
+function test_add(a, b, c){
+  console.log('it should add "' + a + '" and "' + b + '" to equal ' + c + ':',
+  add(a, b) === c);
 }
+
+test_add("one", "one", 2);
+test_add("one", "two", 3);
+test_add("one", "three", 4);
+test_add("one", "four", 5);
+test_add("one", "five", 6);
+test_add("one", "six", 7);
+test_add("one", "seven", 8);
+test_add("one", "eight", 9);
+test_add("one", "nine", 10);
+test_add("one", "ten", 11);
+test_add("two", "one", 3);
+test_add("two", "two", 4);
+test_add("two", "three", 5);
+test_add("two", "four", 6);
+test_add("two", "five", 7);
+test_add("two", "six", 8);
+test_add("two", "seven", 9);
+test_add("two", "eight", 10);
+test_add("two", "nine", 11);
+test_add("two", "ten", 12);
+test_add("three", "one", 4);
+test_add("three", "two", 5);
+test_add("three", "three", 6);
+test_add("three", "four", 7);
+test_add("three", "five", 8);
+test_add("three", "six", 9);
+test_add("three", "seven", 10);
+test_add("three", "eight", 11);
+test_add("three", "nine", 12);
+test_add("three", "ten", 13);
+
 
 
 console.log('it should add "one" and "one":',
