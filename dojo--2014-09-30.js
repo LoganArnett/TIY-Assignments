69caa5428ba30913b82092e3ac0ec34520b5ac92
Logan Arnett <logan@loganarnett.com>
2014-09-30 21:21:56 -0400

    @LoganArnett & @rgcoders L2N

--- dojo--2014-09-30.js
+++ dojo--2014-09-30.js
@@ -13,6 +13,41 @@
  * ping-pong pairing, and commit often using `@username & @username` as the
  * commit message.
  */
+var letters2Numbers = function(letters){
+  if (letters == "one"){
+    letters = 1;
+  }
+  if (letters == "two"){
+    letters = 2;
+  }
+  if (letters == "three"){
+    letters = 3;
+  }
+  if (letters == "four"){
+    letters = 4;
+  }
+  if (letters == "five"){
+    letters = 5;
+  }
+  if (letters == "six"){
+    letters = 6;
+  }
+  if (letters == "seven"){
+    letters = 7;
+  }
+  if (letters == "eight"){
+    letters = 8;
+  }
+  if (letters == "nine"){
+    letters = 9;
+  }
+  if (letters == "ten"){
+    letters = 10;
+  }
+}
+
+
+
 var add = function(a, b){
   if (b === "three"){
     return 5;
@@ -26,6 +61,9 @@ var add = function(a, b){
   if (a === "one"){
     return 2;
   }
+  if (a === "four"){
+    return 9;
+  }
 }
 
 console.log('It should add "one" and "one" to equal 2:',
@@ -36,3 +74,5 @@ console.log('It should add "two" and "two" to equal 4:',
             add("two", "two") === 4);
 console.log('It should add "two" and "three" to equal 5:',
             add("two", "three") === 5);
+console.log('It should add "four" and "five" to equal 9:',
+            add("four", "five") === 9);
