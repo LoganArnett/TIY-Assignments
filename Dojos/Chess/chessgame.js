0f513e136040a286b7d28de0730959b848ddf35f
Logan Arnett <logan@loganarnett.com>
2014-10-15 22:20:51 -0400

    successful testing of moving pieces to destinations

--- Dojos/Chess/chessgame.js
+++ Dojos/Chess/chessgame.js
@@ -12,7 +12,11 @@ function board(x, y){
    ['p','p','p','p','p','p','p','p'],
    ['r','n','b','q','k','b','n','r']
 ];
-};
+}
+
+board.prototype.Pieces = function(x, y){
+
+}
 
 Chess.prototype.getPlayer = function(piece){
   if(piece === piece.toLowerCase()){
@@ -21,10 +25,12 @@ Chess.prototype.getPlayer = function(piece){
      return 'black';
 }
 
-Chess.prototype.move = function(piece, destination){
-     
+Chess.prototype.move = function(a,b,c,d){
+  this.board[c][d] = this.board[a][b];
+  this.board[a][b] = ' ';
+  return this.board;
 }
 
 function Chess(){
   this.board = board();
-};
+}
