3590652dcc60210ae7f4cb322c11c41e925859fa
Logan Arnett <logan@loganarnett.com>
2014-11-13 12:57:17 -0500

    updating links for blog

--- HTML-CSS/gulpfile.js
+++ HTML-CSS/gulpfile.js
@@ -1,20 +1,14 @@
-/*gulpfile with livereload */
 var gulp = require('gulp'),
-    connect = require('gulp-connect');
+    wiredep = require('wiredep');
 
-gulp.task('connect', function() {
-  connect.server({
-    livereload: true
-  });
+require('../../gulpfile.js');
+
+gulp.task('wiredep', function(){
+  wiredep({ src: 'index.html' });
 });
 
-gulp.task('html', function () {
-  gulp.src('./*.html')
-    .pipe(connect.reload());
+gulp.task('bower', function(){
+  gulp.watch('bower.json', [ 'wiredep' ]);
 });
 
-gulp.task('watch', function () {
-  gulp.watch(['./*.html'], ['html']);
-});
-
-gulp.task('default', ['connect', 'watch']);
+gulp.task('default', [ 'connect', 'watch', 'bower' ]);
