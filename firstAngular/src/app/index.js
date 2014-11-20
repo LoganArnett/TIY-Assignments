4f9397f14c1558655f4b094eefb9bb6b79c3a6b3
Logan <logan@loganarnett.com>
2014-11-19 21:42:31 -0500

    got comments active now looking at github api

--- firstAngular/src/app/index.js
+++ firstAngular/src/app/index.js
@@ -1,6 +1,16 @@
+(function(){
 'use strict';
 
-angular.module('myFirstApp', ['ngCookies']);
+  var app = angular.module('myFirstApp', [ ]);
 
-
+  app.controller('CommentController', function(){
+    var list = this;
+    this.comments = [ ];
+    this.newComment = function(comment){
+      this.createdOn = Date.now();
+      this.comments.push(this.comment);
+      
+    };
+  });
 //curl the url for the issue and observe how the comments were pulled from the gist
+})();
