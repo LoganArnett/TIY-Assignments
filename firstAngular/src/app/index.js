c79fa5a1e21631d1def82e183518a645b2b30bf8
Logan <logan@loganarnett.com>
2014-11-19 23:56:32 -0500

    comments working on both pages

--- firstAngular/src/app/index.js
+++ firstAngular/src/app/index.js
@@ -3,14 +3,34 @@
 
   var app = angular.module('myFirstApp', [ ]);
 
-  app.controller('CommentController', function(){
-    var list = this;
-    this.comments = [ ];
-    this.newComment = function(comment){
-      this.createdOn = Date.now();
-      this.comments.push(this.comment);
-      
+  app.controller('CommentController', function($http, $interpolate){
+    var apiUrl = 'https://api.github.com/repos/TheIronYard--Orlando/FEE--2014--FALL/issues/414/comments',
+        apiKeyTpl = $interpolate('?access_token={{key}}');
+
+    var self = this;
+
+    // this.apiKey = '9dbd5217e5eb05668f136a10dee526550e74ba60';
+
+    $http.get(apiUrl)
+      .success(function(data){
+        self.comments = data;
+      });
+
+    this.newComment = function(text, apiKey){
+      var self = this;
+      var url = apiUrl + apiKeyTpl({key: apiKey})
+
+
+
+      $http.post(url, { body : text })
+        .success(function(data){
+          self.comments.push(data);
+        });
+
+      // this.createdOn = Date.now();
+      // this.comments.push(this.comment);
+      // console.log(self.comments);
     };
+
   });
-//curl the url for the issue and observe how the comments were pulled from the gist
 })();
