ecbe59c6571b4400be901f45cc5e3818a78fcef3
Logan <logan@loganarnett.com>
2014-11-24 00:42:34 -0500

    FireApp

--- Firebase/app/scripts/app.js
+++ Firebase/app/scripts/app.js
@@ -1,26 +1,45 @@
 'use strict';
+//
+// /**
+//  * @ngdoc overview
+//  * @name firebaseApp
+//  * @description
+//  * # firebaseApp
+//  *
+//  * Main module of the application.
+//  */
+// angular.module('firebaseApp', [
+//     'firebase',
+//     'firebase.utils',
+//     'simpleLogin'
+//   ]);
+//
+// var app = angular.module("instaComments", ["firebase"]);
+//
+// app.controller("MessageController", function($scope, $firebase) {
+//   var ref = new Firebase("https://instacomments.firebaseio.com/messages");
+//   var sync = $firebase(ref);
+//   $scope.messages = sync.$asArray();
+//   $scope.addMessage = function(text) {
+//     $scope.messages.$add({text: text});
+//   }
+// });
+var fireApp = angular.module("fireApp", ["firebase"]);
 
-/**
- * @ngdoc overview
- * @name firebaseApp
- * @description
- * # firebaseApp
- *
- * Main module of the application.
- */
-angular.module('firebaseApp', [
-    'firebase',
-    'firebase.utils',
-    'simpleLogin'
-  ]);
+fireApp.controller('FireController', ['$scope', '$firebase',
 
-var app = angular.module("instaComments", ["firebase"]);
-
-app.controller("MessageController", function($scope, $firebase) {
-  var ref = new Firebase("https://instacomments.firebaseio.com/messages");
-  var sync = $firebase(ref);
-  $scope.messages = sync.$asArray();
-  $scope.addMessage = function(text) {
-    $scope.messages.$add({text: text});
+  function($scope, $firebase) {
+    var ref = new Firebase("https://instacomments.firebaseio.com/");
+    $scope.messages = $firebase(ref).$asArray();
+    //ADD MESSAGE METHOD
+    $scope.newMessage = function() {
+      //LISTEN FOR RETURN KEY
+      //if (e.keyCode === 13 && \$scope.msg) {
+        //ALLOW CUSTOM OR ANONYMOUS USER NAMES -->
+        var name = $scope.name || 'A Ghost!!!';
+        $scope.messages.$add({from: "-" + name, body: $scope.chats});
+        //RESET MESSAGE
+        $scope.chats = "";
+    }
   }
-});
+]);
